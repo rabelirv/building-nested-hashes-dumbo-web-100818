682ea0731a060bc3ce2e14e855fbaaf7ac87f51f
Irving E Rabel <rabel606@gmail.com>
2018-09-26 21:06:26 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
+      :hero_friends => [friend_one = {:name => "Benvolio", :age => "17", :attitude => "worried"}, friend_two = {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
         #your hashes here!
       ]
    }, 
@@ -12,9 +12,9 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [friend_one = {:name => "Steven", :age => "30", :attitude => "confused"}, friend_two ={:name => "Nurse", :age => "44", :attitude => "worried"}]
         #your hashes here!
-      ]
+  
    }
 }
 
